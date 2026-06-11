967eb5cf7afae3cc5a8e53b9ab58e9fbf97b55c2
achinthajayaweera <achinthajayaweera26@gmail.com>
2026-06-11 14:37:46 +0530

    v7: add main.js with DOM variable declarations and canvas context setup

---
+++ main.js
@@ -0,0 +1,18 @@
+const saturate = document.getElementById("saturate");
+const contrast = document.getElementById("contrast");
+const brightness = document.getElementById("brightness");
+const sepia = document.getElementById("sepia");
+const grayscale = document.getElementById("grayscale");
+const blurr = document.getElementById("blur");
+const hueRotate = document.getElementById("hue-rotate");
+
+const filters = document.querySelectorAll(".filter");
+const image = document.getElementById("img");
+const imgBox = document.querySelector(".img-box");
+
+const downloadBtn = document.getElementById("download");
+const uploadBtn = document.getElementById("upload");
+const resetBtn = document.getElementById("reset");
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
